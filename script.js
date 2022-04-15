e14f24ff2a4c2a897187fddddd44dd40f6067008
Irfan <70712347+Panzer-Kun@users.noreply.github.com>
2022-04-16 00:35:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let searchBox = document.querySelector('#SearchBox');
+let searchButton = document.querySelector('#SearchBtn');
+let searchResult = document.querySelector('#resultText');
+let resultTitle = document.querySelector('#resultTitle');
+
+window.onload = setAutoComplete;
+
+const searchEngineURL = 'http://127.0.0.1:5000/search';
+
+searchButton.addEventListener('click', click);
+
+function click() {
+    let query = searchBox.value;
+    searchBox.value = '';  
+    console.log(query);
+    let formatted = query.replace(' ', '+');
+
+    fetch(searchEngineURL + '?q=' + formatted)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then(data => {
+        console.log(data);
+        resultTitle.innerText = data['query'];
+        searchResult.innerText = data['retrieved'];
+    })
+}
+
+//"https://raw.githubusercontent.com/Panzer-Kun/SearchEngine/main/AIA_TakafulCombined.csv"
+function setAutoComplete() {
+    fetch("https://panzer-kun.github.io/SearchEngine/AIA_TakafulCombined.csv",
+        {
+            method: "get",
+            headers: {
+                "content-type": "text/csv; charset=UTF-8",
+            },
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.text();
+            }
+        })
+        .then(data => {
+            console.log(data);
+        })
+}
