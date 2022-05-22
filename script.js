025739d10521bc27eec08d31cd558e0e214393b8
Irfan <70712347+Panzer-Kun@users.noreply.github.com>
2022-05-22 16:14:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let csvData = [];
 
 window.onload = setAutoComplete;
 
-const searchEngineURL = 'https://abad-103-5-183-41.ap.ngrok.io/search';
+const searchEngineURL = ' https://becc-103-5-183-41.ap.ngrok.io/search';
 
 searchButton.addEventListener('click', click);
 
