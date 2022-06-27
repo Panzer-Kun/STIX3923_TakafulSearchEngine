928402e2e98c21b2334a7358d07a9ef6b9ade2df
Irfan <70712347+Panzer-Kun@users.noreply.github.com>
2022-06-27 17:51:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let csvData = [];
 
 window.onload = setAutoComplete;
 
-const searchEngineURL ='https://0804-103-5-183-48.ap.ngrok.io/search';
+const searchEngineURL ='https://c799-103-5-183-48.ap.ngrok.io/search';
 
 searchButton.addEventListener('click', click);
 
